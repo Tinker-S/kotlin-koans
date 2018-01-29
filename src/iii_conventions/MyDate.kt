3ea318e96be4501e9adc9f204f0ee548d4b34946
ts <ts@mobvoi.com>
2018-01-29 12:20:11 +0800

    My resolution -- iii.

--- src/iii_conventions/MyDate.kt
+++ src/iii_conventions/MyDate.kt
@@ -1,8 +1,24 @@
 package iii_conventions
 
-data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int)
+data class MyDate(val year: Int, val month: Int, val dayOfMonth: Int) : Comparable<MyDate> {
 
-operator fun MyDate.rangeTo(other: MyDate): DateRange = todoTask27()
+    override fun compareTo(other: MyDate) = when {
+        year != other.year -> year - other.year
+        month != other.month -> month - other.month
+        else -> dayOfMonth - other.dayOfMonth
+    }
+}
+
+class RepeatedTimeInterval(val timeInterval: TimeInterval, val number: Int)
+
+operator fun TimeInterval.times(number: Int) = RepeatedTimeInterval(this, number)
+
+operator fun MyDate.rangeTo(other: MyDate): DateRange = DateRange(this, other)
+
+operator fun MyDate.plus(timeInterval: TimeInterval): MyDate = addTimeIntervals(timeInterval, 1)
+
+operator fun MyDate.plus(repeatedTimeInterval: RepeatedTimeInterval): MyDate
+        = addTimeIntervals(repeatedTimeInterval.timeInterval, repeatedTimeInterval.number)
 
 enum class TimeInterval {
     DAY,
@@ -10,4 +26,20 @@ enum class TimeInterval {
     YEAR
 }
 
-class DateRange(val start: MyDate, val endInclusive: MyDate)
+class DateIterator(val dateRange: DateRange) : Iterator<MyDate> {
+    var current: MyDate = dateRange.start
+
+    override fun next(): MyDate {
+        val result = current
+        current = current.addTimeIntervals(TimeInterval.DAY, 1)
+        return result
+    }
+
+    override fun hasNext(): Boolean = current <= dateRange.endInclusive
+}
+
+class DateRange(override val start: MyDate, override val endInclusive: MyDate) : ClosedRange<MyDate>, Iterable<MyDate> {
+    override fun iterator(): Iterator<MyDate> = DateIterator(this)
+
+    override fun contains(value: MyDate): Boolean = start <= value && value <= endInclusive
+}
